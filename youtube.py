f86589ebbc71ac8c89fc3015274f6f9e1981b7cf
Rudra Pratap <119649177+0xDev-Mn@users.noreply.github.com>
2026-02-10 01:42:25 +0530

    Add files via upload

---
+++ youtube.py
@@ -0,0 +1,69 @@
+# from pytube import YouTube
+# import tkinter as tk
+# from tkinter import filedialog
+
+# def download_video(url, save_path):
+#     try:
+#         yt = YouTube(url)
+#         stream = yt.streams.filter(progressive = True, file_extension = "mp4")
+#         highest_res_stream = streams.get_highest_resolution()
+#         highest_res_stream.download(output_path = save_path)
+#         print("Video Downloaded!")
+#     except Exception as e:
+#         print(e)
+
+# def open_file_dialog():
+#     folder = filedialog.askdirectory()
+#     if folder:
+#         print(f"Selected folder: {folder}")
+#     return folder
+
+# if __name__ == "__main__":
+#     root = tk.Tk()
+#     root.withdraw()
+
+#     video_url = input("Enter a YouTube url: ")
+#     save_dir = open_file_dialog()
+
+#     if save_dir:
+#         print("Download started...")
+#         download_video(video_url, save_dir)
+#     else:
+#         print("Invalid folder")
+    
+import tkinter as tk
+from tkinter import filedialog
+import yt_dlp
+
+def download_video(url, save_path):
+    ydl_opts = {
+        "outtmpl": f"{save_path}/%(title)s.%(ext)s",
+        "format": "bestvideo+bestaudio/best",
+        "merge_output_format": "mp4",
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        print("Video Downloaded!")
+    except Exception as e:
+        print(e)
+
+def open_file_dialog():
+    folder = filedialog.askdirectory()
+    if folder:
+        print(f"Selected folder: {folder}")
+    return folder
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()
+
+    video_url = input("Enter a YouTube url: ")
+    save_dir = open_file_dialog()
+
+    if save_dir:
+        print("Download started...")
+        download_video(video_url, save_dir)
+    else:
+        print("Invalid folder")
